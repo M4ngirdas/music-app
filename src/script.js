936bca4e2820ba7dd9643e3126990607f7735c11
M4ngirdas <mangirdasrvz@gmail.com>
2025-03-23 20:19:37 +0200

    changes 03-23

--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
+document.getElementById("download-btn").onclick = function () {
+    window.location.href = "/secondary/download.html";
+}
 
+document.getElementById("app-btn").onclick = function () {
+    window.location.href = "/secondary/app.html"
+}
